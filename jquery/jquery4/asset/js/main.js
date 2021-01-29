2ee3619e92a57b493a112734e4775679740517c9
quidelantoine <quidelantoine@gmail.com>
2021-01-29 16:31:16 +0100

    jour3 jquery

--- jquery/jquery4/asset/js/main.js
+++ jquery/jquery4/asset/js/main.js
@@ -5,18 +5,45 @@ $(document).ready(function(){
   $('#times').on('click', function(){
     $('#menu_responsive').fadeOut(500);
   })
-
 })
 
+// for (var i = 1; i <= 100; i++) {
+//   console.log(i)
+// }
+// for (var i = 0; i <= 100; i+=5) {
+//   console.log(i)
+// }
 
-// window.onscroll = function(ev) {
-//     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-//         console.log('ok bottom')
-//         if(!$('#content').hasClass('onedo')) {
-//           for (var i = 237; i < 243; i++) {
-//             $('#content').append('<img src="https://picsum.photos/id/'+i+'/200/300">')
-//           }
-//         }
-//         $('#content').addClass('onedo')
-//     }
-// };
+$(window).on('load',function() {
+  $('#diapo1').flexslider({
+    controlNav: true,
+    directionNav: true,
+    slideshowSpeed: 15000,
+    animation: "slide",
+    // direction: "vertical",
+    after: function(){
+      console.log('dede')
+    },
+  });
+
+  $('#diapo2').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 400,
+    itemMargin: 5,
+    controlNav: false,
+    directionNav: true,
+  });
+});
+
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    console.log('ok bottom')
+    if(!$('#content').hasClass('onedo')) {
+      for (var i = 237; i < 243; i++) {
+        $('#content').append('<img src="https://picsum.photos/id/'+i+'/200/300">')
+      }
+    }
+    $('#content').addClass('onedo')
+  }
+};
